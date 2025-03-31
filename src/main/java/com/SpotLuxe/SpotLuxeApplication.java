678d611c46288db625ce1533173f3a381cb8c08e
ZIOnysus <1628313654@qq.com>
2025-03-31 12:28:35 +0800

    Initial commit

--- src/main/java/com/SpotLuxe/SpotLuxeApplication.java
+++ src/main/java/com/SpotLuxe/SpotLuxeApplication.java
@@ -1,12 +1,10 @@
 package com.SpotLuxe;
 
-import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@Slf4j
 @EnableAspectJAutoProxy(exposeProxy = true)
 @MapperScan("com.SpotLuxe.mapper")
 @SpringBootApplication
@@ -16,3 +14,4 @@ public class SpotLuxeApplication {
         SpringApplication.run(SpotLuxeApplication.class, args);
     }
 
+}
--- src/main/java/com/SpotLuxe/controller/ShopTypeController.java
+++ src/main/java/com/SpotLuxe/controller/ShopTypeController.java
@@ -2,15 +2,12 @@ package com.SpotLuxe.controller;
 
 
 import com.SpotLuxe.dto.Result;
-import com.SpotLuxe.entity.ShopType;
 import com.SpotLuxe.service.IShopTypeService;
-import com.SpotLuxe.service.impl.ShopServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * <p>
